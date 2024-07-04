a9b020e1687b0e41213de45c3c4f30e226e5d3ff
miriamsobrino <miriamsobrinodev@gmail.com>
2024-07-04 13:35:54 +0200

    change origin cors

--- app.js
+++ app.js
@@ -5,8 +5,6 @@ import User from './models/user.js';
 import Article from './models/article.js';
 import cors from 'cors';
 import multer from 'multer';
-import path from 'path';
-import fs from 'fs';
 import jwt from 'jsonwebtoken';
 import cookieParser from 'cookie-parser';
 import admin from 'firebase-admin';
@@ -40,7 +38,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: '*',
+    origin: 'http://localhost:5173',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
