c770816e155e75584cd0499e6a9c1f936bc20f61
miriamsobrino <miriamsobrinodev@gmail.com>
2024-07-04 12:36:49 +0200

    update cors

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: '*',
+    origin: 'http://localhost:5173',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
