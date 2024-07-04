5680a9cf86d48a9c72731a808bc6c1ad5a3b94ca
miriamsobrino <miriamsobrinodev@gmail.com>
2024-07-04 13:30:25 +0200

    change origin cors

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.use(express.json());
 app.use(fileUpload());
 app.use(
   cors({
-    origin: 'http://localhost:5173',
+    origin: '*',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
