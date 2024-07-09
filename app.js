37b895c93d01a326ca35a6c38f54d39256b3a1cb
miriamsobrino <miriamsobrinodev@gmail.com>
2024-07-09 13:39:46 +0200

    change origin cors

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://ines-sobrino.vercel.app/',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
